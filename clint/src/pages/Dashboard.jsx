70598198c4bfb27aaa3bb8f04d6758fa8c9e4659
Divyamsirswal <divyamsirswal361@gmail.com>
2023-11-24 19:37:53 +0530

    com

--- clint/src/pages/Dashboard.jsx
+++ clint/src/pages/Dashboard.jsx
@@ -1,5 +1,11 @@
+import React, { useState } from "react";
+import "./Dashboard.css";
+import { Link } from "react-router-dom";
+
 export function Dashboard() {
   return (
-    <div>Dashboard Page</div>
-  )
+    <div className="dashboard-container">
+      <Link to={"/chat"}>CHAT PAGE</Link>
+    </div>
+  );
 }
--- clint/src/pages/Home.jsx
+++ clint/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import NavBar from "../components/NavBar";
+import { Dashboard } from "./Dashboard";
 
 export default function Home() {
   return (
     <div>
-        <NavBar />
+      <Dashboard />
     </div>
   );
 }
