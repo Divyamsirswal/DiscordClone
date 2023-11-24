464d84cc62420a5c420be14ada5597f40d15791c
Divyamsirswal <divyamsirswal361@gmail.com>
2023-11-24 16:51:41 +0530

    css added

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 import { Dashboard } from "./pages/Dashboard";
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import Comunity from "./pages/Comunity";
 
 function App() {
   return (
-  <div> 
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='*' element={<h1>Not Found</h1>} />
-        <Route path='/dashboard' element={<Dashboard />} />
-    </Routes>
-  </div>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/comunity" element={<Comunity />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
